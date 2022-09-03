b4a77da59e6c80908835cc9f889e61f54a1b604b
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-09-04 00:54:08 +0300

    changes

--- src/services/productReviewService.ts
+++ src/services/productReviewService.ts
@@ -1,45 +1,48 @@
-import { NotFoundError } from "../helpers/apiError";
-import ProductReview, { ProductReviewDocument } from "../models/ProductReview";
+import { NotFoundError } from '../helpers/apiError'
+import ProductReview, { ProductReviewDocument } from '../models/productReview'
 
 const createOne = async (productReview: ProductReviewDocument) => {
-    return await productReview.save()
+  return await productReview.save()
 }
 
 const findAll = async () => {
-    return await ProductReview.find()
+  return await ProductReview.find()
 }
 
 const findById = async (id: string) => {
-    const foundOne = await ProductReview.findById(id)
-    if (foundOne) {
-        return foundOne
-    } else {
-        throw new NotFoundError()
-    }
+  const foundOne = await ProductReview.findById(id)
+  if (foundOne) {
+    return foundOne
+  } else {
+    throw new NotFoundError()
+  }
 }
 
-const updateOne = async (id: string, update: Partial<ProductReviewDocument>) => {
-    const foundOne = await ProductReview.findByIdAndUpdate(id, update)
-    if (foundOne) {
-        return foundOne
-    } else {
-        throw new NotFoundError()
-    }
+const updateOne = async (
+  id: string,
+  update: Partial<ProductReviewDocument>
+) => {
+  const foundOne = await ProductReview.findByIdAndUpdate(id, update)
+  if (foundOne) {
+    return foundOne
+  } else {
+    throw new NotFoundError()
+  }
 }
 
-const deleteOne =async (id:string) => {
-    const foundOne = await ProductReview.findByIdAndDelete(id)
-    if (foundOne) {
-        return foundOne
-    } else {
-        throw new NotFoundError()
-    }
+const deleteOne = async (id: string) => {
+  const foundOne = await ProductReview.findByIdAndDelete(id)
+  if (foundOne) {
+    return foundOne
+  } else {
+    throw new NotFoundError()
+  }
 }
 
 export default {
-    createOne,
-    findAll,
-    findById,
-    updateOne,
-    deleteOne
+  createOne,
+  findAll,
+  findById,
+  updateOne,
+  deleteOne,
 }
