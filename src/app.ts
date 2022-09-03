f66fffe6192a1e9284e804ec9f0df8a5b9048145
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-09-04 02:51:19 +0300

    Added homw route

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import userReviewRoute from './routers/userReviewRoute'
 import cors from 'cors'
 import passport, { session } from 'passport'
 import { jwtStrategy } from './config/passport'
+import homeRoute from './routers/homeRoute'
 
 dotenv.config({ path: '.env' })
 const app = express()
@@ -32,6 +33,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 // Set up routers
+app.use('api/v1', homeRoute)
 app.use('/api/v1/products', product)
 app.use('/api/v1/users', user)
 app.use('/api/v1/orders', order)
