edc32c9ac90e2a79ee174f29e3f2f0cb69b1c0cb
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-09-04 03:44:13 +0300

    changesa

--- test/db-helper.ts
+++ test/db-helper.ts
@@ -3,8 +3,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 const connect = async () => {
   const mongod = await MongoMemoryServer.create()
-  const uri = await mongod.getUri()
-
+  const uri = mongod.getUri()
   // const mongooseOpts: ConnectOptions = {
   //   useNewUrlParser: true,
   //   useUnifiedTopology: true,
@@ -12,7 +11,7 @@ const connect = async () => {
   //   useCreateIndex: true,
   // }
 
-  await mongoose.connect(uri)
+  await mongoose.connect(await uri)
 
   return {
     closeDatabase: async () => {
